e8bfa59b4d2ae1dfab9d754d4f6a76fa9a220464
211 <211@localhost.localdomain>
2019-11-12 16:34:51 +0900

    예제 6-2)

--- EchoClient.c
+++ EchoClient.c
@@ -13,39 +13,42 @@ int main(){
 	int c_socket; 
 	struct sockaddr_in c_addr;
 	int n;
-	char message[BUFSIZE];
-
+	char rcvBuffer[BUFSIZE];//서버에서 보내준 메세지를 저장하는 변수
+	char sendBuffer[BUFSIZE] = "Hi, I'm client.";
 	//1. 클라이언트 소켓 생성
-	c_socket = socket(PF_INET, SOCK_STREAM, 0);
-
+	c_socket = socket(PF_INET, SOCK_STREAM, 0); //서버와 동일한 설정으로 생성
 	//2.소켓 정보 초기화
 	memset(&c_addr, 0, sizeof(c_addr));
-	c_addr.sin_addr.s_addr = inet_addr(IPADDR);
+	c_addr.sin_addr.s_addr = inet_addr(IPADDR); //접속할 IP 설정 (127.0.0.1)
 	c_addr.sin_family = AF_INET;
 	c_addr.sin_port = htons(PORT);
-	
 	//3. 서버에 접속
-	if(connect(c_socket, (struct sockaddr *) &c_addr, sizeof(c_addr)) == -1){ 
-		printf("Cannot Connect\n");
-		close(c_socket);
-		return -1;
+	if(connect(c_socket, (struct sockaddr *) &c_addr, sizeof(c_addr)) == -1){
+		//서버 접속에 실패하면 
+		printf("Cannot Connect\n"); //Cannot connect 메세지 출력
+		close(c_socket); //자원 회수
+		return -1;  //프로세스 종료
 	}
-
-	//4. 서버에서 보낸 메시지 읽기 
 	while(1){
-		fputs("메시지 입력 : ", stdout);
-		fgets(message,sizeof(message), stdin);
-		message[strlen(message)-1] = '\0';
-		write(c_socket, message, strlen(message));
-		if(strncasecmp(message, "quit", 4) == 0 || strncasecmp(message, "kill server", 11) == 0)
+		//4. 서버에 메시지 보내기 
+		//키보드로부터 메세지 입력 받기
+		fgets(sendBuffer, sizeof(sendBuffer), stdin);
+		//서버로 메세지 전송
+		write(c_socket, sendBuffer, strlen(sendBuffer));
+		//입력받은 메세지가 quit 이면  break
+		if(strncasecmp(sendBuffer, "quit", 4) == 0 || strncasecmp(sendBuffer, "kill server", 11) == 0)
 			break;
-		n = read(c_socket, message, sizeof(message)); 
-		if (n < 0){
+		//5. 서버에서 보낸 메시지 읽기 
+		n = read(c_socket, rcvBuffer, sizeof(rcvBuffer)); 
+		//서버에서 보내준 메세지를 rcvBuffer에 저장하고, 메세지의 길이를 리턴
+		//만약 read에 실패하면, -1을 리턴
+		if (n < 0){ //read() 함수 실패 
 			printf("Read Failed\n");
 			return -1;
 		}
-		message[n] = '\0'; //문자열 뒷부분 깨짐 방지
-		printf("메시지 받음: %s\n", message); //서버에서 받은 메세지 출력
+		rcvBuffer[n] = '\0'; //문자열 뒷부분 깨짐 방지
+		printf("received data: %s\n", rcvBuffer);
+		printf("rcvBuffer length: %d\n", n);
 	}
 	close(c_socket);
 	return 0;	
