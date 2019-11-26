a25643c09dc6221c39298c9a59948e8cfd0a4a4a
211 <211@localhost.localdomain>
2019-11-26 16:09:08 +0900

    멀티스레드 기반 채팅프로그램 기본채팅&귓속말

--- MultiThreadChatClient.c
+++ MultiThreadChatClient.c
@@ -7,13 +7,17 @@
 #include <sys/select.h>
 #include <pthread.h>
 #include <signal.h>
+
 #define CHATDATA 1024
 #define IPADDR "127.0.0.1"
 #define PORT 9000
+#define WHISPER "/w"
+#define DELIMETER " "
 
 void *do_send_chat(void *); //두개의 스레드 생성
 void *do_receive_chat(void *);
 pthread_t thread_1, thread_2;
+
 char    escape[ ] = "exit";
 char    nickname[20];
 
@@ -39,12 +43,14 @@ int main(int argc, char *argv[ ])
         printf("Can not connect\n");
         return -1;
     }
-	//커넥트 되면 
-    //pthread_create with do_send function
-    //pthread_create with do_receive_chat function
-    //pthread_join both threads
-    close(c_socket);
+	write(c_socket, nickname, strlen(nickname));	
+	pthread_create(&thread_1, NULL, do_send_chat, (void *) &c_socket);
+	pthread_create(&thread_2, NULL, do_receive_chat, (void *) &c_socket);
+	pthread_join(thread_1, NULL);
+	pthread_join(thread_2, NULL);
+	close(c_socket);
 }
+
 void * do_send_chat(void *arg)
 {
     char chatData[CHATDATA];
@@ -53,19 +59,36 @@ void * do_send_chat(void *arg)
     int c_socket = *((int *) arg);        // client socket
     while(1) {
         memset(buf, 0, sizeof(buf));
-        if((n = read(0, buf, sizeof(buf))) > 0 ) { //키보드에서 입력 받은 문자열을 buf에 저장. read()함수의 첫번째 인자는 file descriptor로써, 0은 stdin, 즉 키보드를 의미함.
-            sprintf(chatData, "[%s] %s", nickname, buf);
-            write(c_socket, chatData, strlen(chatData)); //서버로 채팅 메시지 전달
-            if(!strncmp(buf, escape, strlen(escape))) { //'exit' 메세지를 입력하면,
-                pthread_kill(thread_2, SIGINT); //do_receive_chat 스레드를 종료시킴
-                break; //자신도 종료
+        if((n = read(0, buf, sizeof(buf))) > 0 ) {
+			char *token = NULL;
+			char *toNickname = NULL;
+			char *message = NULL;
+			if(strncasecmp(buf, WHISPER, 2) == 0){
+				token = strtok(buf, DELIMETER);
+				if(token != NULL)
+					toNickname = strtok(NULL, DELIMETER);
+				if(toNickname != NULL)
+					message = strtok(NULL, "\0");
+				if(token == NULL || toNickname == NULL || message ==  NULL){
+					printf("Your whisper message is wrong. Please input '/w nickname message'\n");
+					continue;
+				}
+
+				sprintf(chatData, "%s %s [%s] %s", token, toNickname, nickname, message);
+			} else { 
+				sprintf(chatData, "[%s] %s", nickname, buf);
+			}
+			write(c_socket, chatData, strlen(chatData)); //서버로 채팅 메시지 전달
+			if(!strncmp(buf, escape, strlen(escape))) { //'exit' 메세지를 입력하면
+				pthread_kill(thread_2, SIGINT); //do_receive_char 스레드를 종료시킴
+				break; //자신도 종료
             }
         }
     }
 }
 void *do_receive_chat(void *arg)
 {
-    char    chatData[CHATDATA];
+    char   chatData[CHATDATA];
     int    n;
     int    c_socket = *((int *)arg);        // client socket
     while(1) {
--- MultiThreadChatServer.c
+++ MultiThreadChatServer.c
@@ -7,8 +7,9 @@
 #include <pthread.h>
 
 void *do_chat(void *); //채팅 메세지를 보내는 함수
-int pushClient(int); //새로운 클라이언트가 접속했을 때 클라이언트 정보 추가
+int pushClient(int, char *); //새로운 클라이언트가 접속했을 때 클라이언트 정보 추가
 int popClient(int); //클라이언트가 종료했을 때 클라이언트 정보 삭제
+
 pthread_t thread;
 pthread_mutex_t mutex;
 
@@ -16,25 +17,35 @@ pthread_mutex_t mutex;
 #define CHATDATA 1024
 #define INVALID_SOCK -1
 #define PORT 9000
+#define WHISPER "/w"
+#define DELIMETER " "
 
-int    list_c[MAX_CLIENT]; // 현재 접속하고있는 클라이언트 관리 mutex 필요
+struct chatClient {
+	int c_socket;
+	char nickname[CHATDATA];
+};
+
+struct chatClient list_c[MAX_CLIENT]; // 현재 접속하고있는 클라이언트 관리 mutex 필요
 char    escape[ ] = "exit"; //클라이언트 종료
 char    greeting[ ] = "Welcome to chatting room\n"; //인사메세지
 char    CODE200[ ] = "Sorry No More Connection\n"; //접속이 잘 안됐을 때 , 클라이언트 수 이상으로 접속되었을 경우
+
 
 int main(int argc, char *argv[ ])
 {
     int c_socket, s_socket;
     struct sockaddr_in s_addr, c_addr;
     int    len;
-    int    i, j, n;
+    int    i, n;
     int    res;
+	 char nickname[CHATDATA];
 
     if(pthread_mutex_init(&mutex, NULL) != 0) { //mutex 초기화
         printf("Can not create mutex\n");
         return -1;
     }
-    s_socket = socket(PF_INET, SOCK_STREAM, 0);
+    s_socket = socket(PF_INET, SOCK_STREAM, 0); // 서버소켓 생성
+
     memset(&s_addr, 0, sizeof(s_addr));
     s_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     s_addr.sin_family = AF_INET;
@@ -50,22 +61,31 @@ int main(int argc, char *argv[ ])
         return -1;
     }
 
-    for(i = 0; i < MAX_CLIENT; i++) //리스트 클라이언트 목록을 초기
-        list_c[i] = INVALID_SOCK;화
+    for(i = 0; i < MAX_CLIENT; i++) //리스트 클라이언트 목록을 초기화
+        list_c[i].c_socket = INVALID_SOCK;
 
     while(1) { //accept 대기
         len = sizeof(c_addr);
         c_socket = accept(s_socket, (struct sockaddr *) &c_addr, &len);
-        res = pushClient(c_socket); //현재 접속한 클라이언트 정보를 list에 추가
+
+		if((n = read(c_socket, nickname, sizeof(nickname))) < 0){
+			printf("nickname read fail.\n");
+			return -1;
+		}
+
+		res = pushClient(c_socket, nickname); //현재 접속한 클라이언트 정보를 list에 추가
+
         if(res < 0) { //MAX_CLIENT만큼 이미 클라이언트가 접속해 있다면, 마이너스 값 리턴
             write(c_socket, CODE200, strlen(CODE200));
             close(c_socket);
         } else {
-            write(c_socket, greeting, strlen(greeting));
-            //pthread_create with do_chat function.
-			///////////////////////////////////////////////////////////
-        }
-    }
+			write(c_socket, greeting, strlen(greeting));
+			pthread_create(&thread, NULL, do_chat, (void*)&c_socket);
+           //pthread_create with do_chat function.
+			//스레드 생성 인자값 : 1) 생성된 스레드의 식별자를 저장할 변수의 포인터, 2) 일반적으로 NULL, 3)void *인 함수를 가리키는 포인터
+			//4)스레드에 의해 호출되는 함수에 전달하고자 하는 인자
+        	}
+  	  }
 }
 
 void *do_chat(void *arg) //현재 접속한 해당 클라이언트가 메시지 보내는 걸 읽음
@@ -75,31 +95,62 @@ void *do_chat(void *arg) //현재 접속한 해당 클라이언트가 메시지 
     int i, n;
     while(1) {
         memset(chatData, 0, sizeof(chatData)); //챗데이터에 저장
-        if((n = read(c_socket, chatData, sizeof(chatData))) > 0) {
-            //write chatData to all clients 이부분 코드 적기
-            //
-            ///////////////////////////////
-            if(strstr(chatData, escape) != NULL) { //클라이언트가 보낸 메시지가 exit이면 대화방 나가기
+        if((n = read(c_socket, chatData, sizeof(chatData))) > 0){
+		
+			char *token = NULL;
+			char *nickname = NULL;
+			char *message = NULL;
+
+			if(strncasecmp(chatData, WHISPER, 2) == 0){
+				token = strtok(chatData, DELIMETER); // \w
+				nickname = strtok(NULL, DELIMETER); //nickname
+				message = strtok(NULL, "\0"); 
+			}
+			for(i = 0; i < MAX_CLIENT; i++) {
+				if(list_c[i].c_socket != INVALID_SOCK) {
+					if(nickname != NULL){
+						if(strcasecmp(list_c[i].nickname, nickname) == 0)
+							write(list_c[i].c_socket, message, strlen(message));
+					}else{
+						write(list_c[i].c_socket, chatData, n);
+					}
+				}
+			}
+			if(strstr(chatData, "/exit") != NULL) { //클라이언트가 보낸 메시지가 exit이면 대화방 나가기
                 popClient(c_socket); //클라이언트 목록에서 삭제
                 break;
-            }
-        }
-    }
+          		 }
+       		 }
+    	}
 }
 
-int pushClient(int c_socket) { //클라이언트가 들어오면 추가
-    //ADD c_socket to list_c array.
-    //
-    ///////////////////////////////
-    //return -1, if list_c is full.
-    //return the index of list_c which c_socket is added.
-}
+int pushClient(int c_socket, char *nickname) {
+	int i;
+	for(i = 0; i < MAX_CLIENT; i++) {
+		if(list_c[i].c_socket == INVALID_SOCK) {
+			pthread_mutex_lock(&mutex);
+			list_c[i].c_socket = c_socket;
+			memset(list_c[i].nickname, 0, CHATDATA);
+			strcpy(list_c[i].nickname, nickname);
+			pthread_mutex_unlock(&mutex);
+			return i;
+		}
+	}
+	if(i == MAX_CLIENT)
+		return -1;
+}	
 
-int popClient(int c_socket) //클라이언트 삭제
-{
-    close(c_socket);
-    //REMOVE c_socket from list_c array.
-    //
-    ///////////////////////////////////
+int popClient(int c_socket)
+{			
+	int i;	 
+	close(c_socket); 
+	for(i = 0; i < MAX_CLIENT; i++) {
+		if(c_socket == list_c[i].c_socket) {
+			pthread_mutex_lock(&mutex);
+			list_c[i].c_socket = INVALID_SOCK;
+			pthread_mutex_unlock(&mutex);
+			break;
+		}
+	}
+	return 0;
 }
-
